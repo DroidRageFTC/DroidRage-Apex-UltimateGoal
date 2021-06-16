2418d05a4463469bdb8e6213bfbb698c85373499
Your Name <you@example.com>
2021-06-16 08:45:23 -0500

    enabled back and forth and added delay intake and regular intake autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/delay/blue/left/BlueLeftZeroCommand.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.auton.delay.blue.left;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.commands.PlaceWobbleGoal;
+import org.firstinspires.ftc.teamcode.commands.drive.DriveForwardCommand;
+import org.firstinspires.ftc.teamcode.commands.drive.TurnCommand;
+import org.firstinspires.ftc.teamcode.commands.drive.TurnToCommand;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterFeeder;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterWheels;
+import org.firstinspires.ftc.teamcode.subsystems.Vision;
+import org.firstinspires.ftc.teamcode.subsystems.WobbleGoalArm;
+
+import org.firstinspires.ftc.teamcode.auton.values.outside.OutsideZeroValues;
+
+public class BlueLeftZeroCommand extends SequentialCommandGroup {
+    public BlueLeftZeroCommand(Drivetrain drivetrain, ShooterWheels shooterWheels, ShooterFeeder feeder, Intake intake, WobbleGoalArm wobbleGoalArm, Vision vision, Telemetry telemetry) {
+        final int HG_SPEED = 360;
+        final int POWERSHOT_SPEED = 3000;
+
+        OutsideZeroValues distance = new OutsideZeroValues();
+        OutsideZeroValues angle = new OutsideZeroValues();
+
+        addCommands(
+                
+                
+                new BlueLeftShootingSequence(drivetrain, shooterWheels, feeder),
+
+                //Place Wobble Goal
+                new DriveForwardCommand(drivetrain, distance.distanceOne),
+                new TurnCommand(drivetrain, -angle.angleOne),
+                new DriveForwardCommand(drivetrain, distance.distanceTwo),
+                new PlaceWobbleGoal(wobbleGoalArm),
+                new DriveForwardCommand(drivetrain, distance.distanceThree)
+                );
+    }
+}
+    //new SplineCommand(drivetrain, new Vector2d(-42, 12), Math.toRadians(180)),
+    //new DriveForwardCommand(drivetrain, -40),
+    //new TurnToCommand(drivetrain, 170),
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/opmode/BackAndForth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/opmode/BackAndForth.java
@@ -31,7 +31,6 @@ import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
  * is recommended that you use the FollowerPIDTuner opmode for further fine tuning.
  */
 @Config
-@Disabled
 @Autonomous(group = "drive")
 public class BackAndForth extends MatchOpMode {
 
